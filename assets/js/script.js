412c0c6ef9c8186cee77bdb1410f676cd9505010
joemardev <joemarb.dev@gmail.com>
2024-01-12 15:46:37 +0800

    Fix the Footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ const swiper = new Swiper(".swiper", {
 	// direction: "vertical",
 	speed: 500,
 	loop: true,
+	autoplay: {
+		delay: 1000,
+	},
 	centeredSlides: true,
 	slidesPerView: "auto",
 	// If we need pagination
@@ -26,30 +29,14 @@ const swiper = new Swiper(".swiper", {
 	},
 });
 
-const footerSwiper = new Swiper(".footer-swiper", {
-	// Optional parameters
-	// direction: "vertical",
-	speed: 500,
+const footerSwiper = new Swiper(".footer-marquee", {
+	speed: 3000,
+	allowTouchMove: false,
+	autoplay: {
+		delay: 0,
+	},
+	freeMode: true,
 	loop: true,
-	centeredSlides: true,
-	slidesPerView: "auto",
-	// If we need pagination
-	pagination: {
-		el: ".swiper-pagination",
-		type: "custom", // use custom rendering
-		renderCustom: function (swiper, current, total) {
-			return current + " / " + total; // format it as 'current/total'
-		},
-	},
-
-	// Navigation arrows
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-
-	// And if we need scrollbar
-	scrollbar: {
-		el: ".swiper-scrollbar",
-	},
+	spaceBetween: 20,
+	slidesPerView: 8,
 });
